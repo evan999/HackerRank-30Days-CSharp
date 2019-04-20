4d959586f98aae1cc2d125a5c8963aac94d82582
evan999 <emeshberge@gmail.com>
2019-04-19 23:52:10 -0400

    Solution compiles and runs successfully.

--- PrintEvenAndOddIndex.cs
+++ PrintEvenAndOddIndex.cs
@@ -4,22 +4,34 @@ using System.IO;
 class Solution {
     static void Main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
-       int T = Convert.ToInt32(Console.ReadLine());
+        int T = Convert.ToInt32(Console.ReadLine());
+        
 
         for (int i = 0; i < T; i++)
-        {  
+        {
             string s = Console.ReadLine();
             char[] chars = s.ToCharArray();
+            //string t = "";
 
             for (int j = 0; j < chars.Length; j++)
             {
                 if (j % 2 == 0)
                 {
+                    //t = t + chars[j];
                     Console.Write(chars[j]);
                 }
             }
 
+            Console.Write(" ");
+
+            for (int k = 0; k < chars.Length; k++)
+            {
+                if (k % 2 != 0)
+                {
+                    Console.Write(chars[k]);
+                }
+            }
             Console.WriteLine();
-        }
-		}
+        }        
+    }
 }
